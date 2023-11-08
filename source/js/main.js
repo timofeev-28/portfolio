0a39369ddbe5721525c71d15e46d9f0080427ec6
Dmitrii Timofeev <28dngn@gmail.ru>
2023-11-08 04:22:12 +0300

    добавлена ссылка на работу supergym

--- source/js/main.js
+++ source/js/main.js
@@ -3,9 +3,11 @@ import { initTabs } from "./tabs.js"
 import { initDropdown } from "./dropdown.js"
 import { initDropdownFooter } from "./dropdown-footer.js"
 import { createGameCells } from "./cells.js"
+import { getNumber } from "./number.js"
 
 initBurger()
 initTabs()
 initDropdown()
 initDropdownFooter()
 createGameCells()
+getNumber()
